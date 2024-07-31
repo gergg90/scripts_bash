909dbfa3634e727fe981de846b6c4c8393581dc8
gergg90 <germain.gg90@gmail.com>
2024-07-31 17:35:19 -0400

    se agrega script htbmachines

---
+++ htbmachines/htbmachine.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+
+function ctrl_c(){
+    echo -e "\n\n ${redColour}[+]Saliendoo...${endColour}"
+    exit 1
+}
+# Ctrl_c
+trap ctrl_c INT
+
+function helpPanel(){
+    echo -e "\n[+]Uso: \n"
+}
+
+function searchMachine(){
+    echo "Esta es la maquina: $1"
+}
+
+# Indicadores
+declare -i parameter_counter=0
+
+
+while getopts "m:h" arg; do
+    case $arg in
+        m) machineName=$OPTARG;  let parameter_counter+=1
+        ;;
+        h)
+        ;;
+    esac
+done
+
+if [ $parameter_counter -eq 1 ]; then
+    
+    searchMachine $machineName
+    
+else
+    
+    helpPanel
+    
+fi
