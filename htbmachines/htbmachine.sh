edd7ec6569bcd9969fcdcd9bae4396d600696868
gergg90 <germain.gg90@gmail.com>
2024-07-31 23:28:28 -0400

    actualizaciones de descarga

--- htbmachines/htbmachine.sh
+++ htbmachines/htbmachine.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# variables_blobales
+main_url="https://htbmachines.github.io/bundle.js"
+
 #Colors
 greenColor="\e[0;32m\033[1m"
 endColor="\033[0m\e[0m"
@@ -7,21 +10,22 @@ redColor="\e[0;31m\033[1m"
 blueColor="\e[0;34m\033[1m"
 yellowColor="\e[0;33m\033[1m"
 purpleColor="\e[0;35m\033[1m"
-turquoiseColor="\e[0;36m\033[1m"
+turquesaColor="\e[0;36m\033[1m"
 grayColor="\e[0;37m\033[1m"
 
 function ctrl_c() {
     echo -e "\n\n ${redColor}[+]Saliendoo...${endColor}"
-    exit 1
+    tput cnorm && exit 1
 }
-# Ctrl_c
+# Ctrl_c >> cancelador
 trap ctrl_c INT
 
+# funciones
 function helpPanel() {
     echo -e "\n${yellowColor}[+]${endColor}${grayColor}Uso:${endColor}"
-    echo -e "\t${purpleColor}u)${endColor}${grayColor} Update or download files${endColor}"
-    echo -e "\t${purpleColor}m)${endColor}${grayColor} Search a machine name${endColor}"
-    echo -e "\t${purpleColor}h)${endColor} ${grayColor}Show help panel${endColor}\n"
+    echo -e "\t${purpleColor}u)${endColor}${grayColor} Actualiza o descarga el archivo${endColor}"
+    echo -e "\t${purpleColor}m)${endColor}${grayColor} Buscador de maquinas ${endColor}"
+    echo -e "\t${purpleColor}h)${endColor} ${grayColor} Mostrar panel de ayuda${endColor}\n"
 
 }
 
@@ -30,12 +34,41 @@ function searchMachine() {
 }
 
 function updateFiles() {
-    echo -e "\n [+] Update Files"
+
+    if [ ! -f bundle.js ]; then
+        tput civis
+        echo -e "\n ${turquesaColor}[+]${endColor}${blueColor} Descargando archivos${endColor}"
+        curl -s $main_url | js-beautify >bundle.js
+        echo -e "\n ${turquesaColor}[+]${endColor}${blueColor} Archivos descargados exitosamente${endColor}\n"
+        tput cnorm
+    else
+        tput civis
+        echo -e "\n ${yellowColor}[+]${endColor} ${grayColor}Comprobando si hay actualizaciones pendientes...${endColor}"
+        sleep 2
+        curl -s $main_url | js-beautify >bundle_temp.js
+        md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
+        md5_original_value=$(md5sum bundle.js | awk '{print $1}')
+
+        if [ "$md5_temp_value" != "$md5_original_value" ]; then
+            echo -e "\n ${yellowColor}[+]${endColor} ${grayColor}Hay actualizaciones${endColor}"
+            rm bundle.js
+            mv bundle_temp.js bundle.js
+            sleep 3
+            echo -e "\n ${yellowColor}[+]${endColor} ${grayColor}Archivos actualizados${endColor}"
+        else
+            echo -e "\n ${yellowColor}[+]${endColor} ${grayColor}No hay actualizaciones pendientes${endColor}"
+            rm bundle_temp.js
+        fi
+
+        tput cnorm
+    fi
+
 }
 
-# Indicadores
+# indicadores
 declare -i parameter_counter=0
 
+# argumentos
 while getopts "m:uh" arg; do
     case $arg in
     m)
@@ -49,6 +82,7 @@ while getopts "m:uh" arg; do
     esac
 done
 
+# controladores de argumentos
 if [ $parameter_counter -eq 1 ]; then
     searchMachine $machineName
 elif [ $parameter_counter -eq 2 ]; then
